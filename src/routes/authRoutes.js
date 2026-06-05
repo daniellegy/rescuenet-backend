6b1b83b4cbadd11d018c61d816e8aa47c45f8fe1
daniellegy <le202332796@alm.buap.mx>
2026-06-04 22:02:35 -0600

    feat: Creamos endpoint (authRoutes)

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { registrarUsuario } = require('../controllers/authController');
+
+// Ruta: POST /api/auth/register
+router.post('/register', registrarUsuario);
+
+module.exports = router;
