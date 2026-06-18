5212f4a96297656fab9d9665b387dc8042fefbb5
Chris5Flov <c.tend145@gmail.com>
2026-06-18 14:00:26 -0600

    rutas a obtener y actualizar perfil

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { registrarUsuario, iniciarSesion } = require('../controllers/authController');
+const { registrarUsuario, iniciarSesion, obtenerPerfil, actualizarPerfil } = require('../controllers/authController');
+const authMiddleware = require('../middlewares/auth');
 
 // Ruta: POST /api/auth/register
 router.post('/register', registrarUsuario);
+
+// Ruta: POST /api/auth/login
 router.post('/login', iniciarSesion);
+
+// Ruta: GET /api/auth/perfil
+router.get('/perfil', authMiddleware, obtenerPerfil);
+
+router.put('/perfil', authMiddleware, actualizarPerfil);
 
 module.exports = router;
